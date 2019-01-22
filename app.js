5f9414cc6f2ce54764a2186380e4bd1166ce64ad
Landry <44202533+Landry728@users.noreply.github.com>
2019-01-22 13:25:30 -0600

    Final commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let friends = ["Ramie", "David", "Manny", "Katie", "Biscuit"];
+
+function addNewFriend() {
+    document.getElementById("inside").value = document.getElementById("inside").value;
+  }
+
+  function addNewFriend(){
+
+    console.log("")
+    }
+
+function push(friendList) {
+ 
+for (f = 0; f < friendList.length; f++){
+    let curFriend = friendList[f]; {
+        for (s = 99; s > 0; s--) {
+            let minusOne = s - 1;
+            if (s > 1) {
+            console.log(s + " lines of code in the file, " + s + " lines of code; " + curFriend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
+            } else {
+            console.log( " 1 line of code in the file, 1 line of code; " + curFriend + " strikes one out, clears it all out, no more lines of code in the file");
+            }
+        }
+    }
+ 
+}}
+
+push(friends)
+addNewFriend();
